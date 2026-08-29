494ed50840d68b5b8be856988c654b12ca899e36
Joxquin <jotajoxquin@gmail.com>
2026-08-28 19:03:43 -0500

    feat(Student): Add funcionality

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,5 +1,35 @@
 public class StudentController {
-	public void controller() {
-		// TO DO	
+	private StudentModel model;
+	private StudentView view;
+	private StudentReport report;
+
+	public StudentController(StudentModel model, StudentView view, StudentReport report) {
+		this.model = model;
+		this.view = view;
+		this.report = report;
+	}
+
+	public void setStudentName(String name) {
+		model.setName(name);
+	}
+
+	public String getStudentName() {
+		return model.getName();
+	}
+
+	public void setStudentId(String id) {
+		model.setId(id);
+	}
+
+	public String getStudentId() {
+		return model.getId();
+	}
+
+	public void updateView() {
+		view.printStudentDetails(model.getName(), model.getId());
+	}
+
+	public void createReport() {
+		report.generateReport(model.getName(), model.getId());
 	}
 }
