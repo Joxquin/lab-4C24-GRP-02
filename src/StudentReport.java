ef119d1bc9ed8d5ba0449ded48ef6f622a1e8261
David Quispe <david.quispe.t@tecsup.edu.pe>
2026-08-28 19:09:02 -0500

    mensaje para ti

---
+++ src/StudentReport.java
@@ -0,0 +1,7 @@
+public class StudentReport {
+    public void generateReport(String studentName, String studentId) {
+        System.out.println("--- REPORTE OFICIAL ---");
+        System.out.println("ID: " + studentId + " - Alumno: " + studentName);
+        System.out.println("-----------------------");
+    }
+}
---
+++ src/StudentView.java
@@ -0,0 +1,7 @@
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentId) {
+        System.out.println("Estudiante:");
+        System.out.println("Nombre: " + studentName);
+        System.out.println("ID: " + studentId);
+    }
+}
